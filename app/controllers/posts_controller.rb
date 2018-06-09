9a5dc978ad30ea6394998bd7b165a925c9f10bba
Robin Doirin <robindoirin@gmail.com>
2018-06-09 03:12:53 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,13 +25,13 @@ end
 
 private
 
-# We pass the permitted fields in as *args;
-# this keeps `post_params` pretty dry while
-# still allowing slightly different behavior
-# depending on the controller action
-def post_params(*args)
-params.require(:post).permit(*args)
-end
+	# We pass the permitted fields in as *args;
+	# this keeps `post_params` pretty dry while
+	# still allowing slightly different behavior
+	# depending on the controller action
+	def post_params(*args)
+		params.require(:post).permit(*args)
+	end
 
 	def edit
 	  @post = Post.find(params[:id])
